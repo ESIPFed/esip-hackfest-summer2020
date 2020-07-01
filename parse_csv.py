bb29947b91d241e0522bf96833ef6b7fa13b2621
Maggie Zhu <maggieqzhu@yahoo.com>
2020-07-01 12:40:56 -0400

    parsing info

--- parse_csv.py
+++ parse_csv.py
@@ -3,17 +3,18 @@ import csv
 '''
 sample csv: file.csv
 
-(Applications - 2 rows)
-name, website, year_started, topic, dataset_doi, conf_level
-Global Flood Monitoring System,http://flood.umd.edu/,2014,Flooding,10.5067/TRMM/TMPA/3H-E/7,High
+(CSV file header)
+topic,name,website,publication,conf-level,identifier
 
+DictReader returns:
+OrderedDict([('topic', 'Flooding'), ('name', 'GFMS')...etc])
 
 '''
 
 with open('file.csv', 'r') as csv_file: 
     csv_reader = csv.DictReader(csv_file) # add , delimiter=',' to specify delimiter
 
-next(csv_reader)  # skips over the header (first line) 
+next(csv_reader)  # skips over both header rows 
 
 for line in csv_reader:
     print(line) # prints "['GFMS', 'http://flood.umd.edu/', '2014'...etc]"
