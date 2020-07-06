83789edbfbafc9102ceb8e5a638a9989bbb09fc2
Maggie Zhu <maggieqzhu@yahoo.com>
2020-07-06 19:10:14 -0400

    neo4j

--- models.py
+++ models.py
@@ -1,12 +1,19 @@
 from py2neo import Graph, Node, Relationship
+import os
 import csv
+
+
+url = os.environ.get('GRAPHENEDB_URL', 'http://localhost:7474')
+username = os.environ.get('NEO4J_USERNAME')
+password = os.environ.get('NEO4J_PASSWORD')
+
 
 def db():
     with open('input.csv', 'r') as csv_file: 
         csv_reader = csv.DictReader(csv_file) # add , delimiter=',' to specify delimiter
 
         # next(csv_reader)  # skips over both header rows 
-        graph = Graph()
+        graph = Graph(url + '/db/data/', username=username, password=password)
         for line in csv_reader:
             topic = Node("Topic", name=line['topic']) # merge later on
             application = Node("Application", name=line['name'], website=line['website'],
